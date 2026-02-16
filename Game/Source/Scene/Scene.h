e61b8b649a69cf95a64ce0a266e4bbe42a7f0062
01254016 <ca01254016@st.kawahara.ac.jp>
2026-02-16 14:38:55 +0900

    GameにPlayerを生成させるコードを追加

--- Game/Source/Scene/Scene.h
+++ Game/Source/Scene/Scene.h
@@ -7,12 +7,16 @@
 
 class Scene :public IGameObject
 {
+protected:
+	SpriteRender m_spriteRender;
 
 
 public:
 	virtual bool Start() override {};
 	virtual void Update() override {};
 	virtual void Render(RenderContext& rc) override {};
+
+
 
 };
 
